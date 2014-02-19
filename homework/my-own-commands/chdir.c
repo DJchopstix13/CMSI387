a66e644e78957df3b015da31d9d97926b7bfcc0e
Jonathan Piatos <jpiatos13@gmail.com>
2014-02-18 23:58:57 -0800

    working on more syscalls

--- homework/my-own-commands/chdir.c
+++ homework/my-own-commands/chdir.c
@@ -13,13 +13,12 @@ int main(int argc, char *argv[]) {
   // We demonstrate the use of command-line arguments here.
   // But note the non-existent error handling (all the better
   // to illustrate the error code below).
-  int result = syscall(39, argv[1], 0777 /* Note: octal */);
+  int result = syscall(12, argv[1], 0777 /* Note: octal */);
 
   // A result of -1 means that something went wrong.  Otherwise,
   // check for your new directory!
   if (result == -1) {
     // Don't use this error message in "real" programs. O_o
-    char *errorMessage = "Herp derp mkderp\n";
     syscall(4, 2, errorMessage, strlen(errorMessage));
   }
 }
