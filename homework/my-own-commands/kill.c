de1eacd9e69bcbb913da2aeaefd756339c437f86
Jonathan Piatos <jpiatos13@gmail.com>
2014-02-18 23:21:21 -0800

    started syscalls

---
+++ homework/my-own-commands/kill.c
@@ -0,0 +1,22 @@
+/* Jonathan Piatos
+** CMSI387: Operating System
+** kill.c
+** Description: This program implements the 
+** "Kill" system call (37) using the syscall function.
+*/
+
+#include <string.h>
+
+
+int main(int argc, char *argv[]){
+    
+    int kill(pid_t pid, int sig); 
+
+    if (/* condition */)
+    {
+    	/* code */
+    }
+
+    syscall(37);
+
+}
