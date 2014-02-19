a2c1720f779657bb1cd726886e879b76bf2f3309
Jonathan Piatos <jpiatos13@gmail.com>
2014-02-19 01:18:47 -0800

    added rename syscall; modified sysinfo to include more iformation about RAM

--- homework/my-own-commands/sysinfo.c
+++ homework/my-own-commands/sysinfo.c
@@ -12,13 +12,13 @@
 #include <stdio.h>
 
 int main(int argc, char *argv[]) {
-  // The sysinfo structure is in kernel.h.
-  struct sysinfo result;
-
-  // We keep the hardcoded system call numbers to illustrate
-  // that they are still just that: numbers.
-  syscall(116, &result);
-
-  // Display some of the returned RAM information.
-  printf("Total RAM: %ld, free RAM: %ld\n", result.totalswap, result.bufferram);
+    // The sysinfo structure is in kernel.h.
+    struct sysinfo result;
+    
+    // We keep the hardcoded system call numbers to illustrate
+    // that they are still just that: numbers.
+    syscall(116, &result);
+    
+    // Display some of the returned RAM information.
+    printf("Total RAM: %ld, free RAM: %ld, shared Ram %ld, buffer Ram %ld\n, total swap %ld, total processes %d, Total high memory size %ld\n", result.totalram, result.freeram, result.sharedram, result.bufferram, result.totalswap, result.procs, result.totalhigh);
 }
