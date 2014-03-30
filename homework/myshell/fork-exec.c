448ad7e24d15e5281d26a2059366fa36ba43673d
Jonathan Piatos <jpiatos13@gmail.com>
2014-03-29 23:36:59 -0700

    modified shell and answered pondering questions

--- homework/myshell/fork-exec.c
+++ homework/myshell/fork-exec.c
@@ -1,36 +1,102 @@
+/* Jonathan Piatos
+** CMSI387: Operating Systems
+** fork-exec.c
+*/
+
 #include <sys/types.h>
 #include <stdio.h>
 #include <unistd.h>
+#include <string.h>
 
 /**
  * This program demonstrates the use of the fork() and exec()
  * functions.
  */
 int main() {
-    /* String to hold the command to run. */
+
+    //String to hold the command to run.
     char command[256];
-    printf("Enter the command to run: ");
-    scanf("%s", command);
+    char *args[256];
+    int i;
 
-    /* Variable that will store the fork result. */
-    pid_t pid;
+    //continuous do-while loop
+    do {
 
-    /* Perform the actual fork. */
-    pid = fork();
-    if (pid < 0) {
-        /* Error condition. */
-        fprintf(stderr, "Fork failed\n");
-        return -1;
-    } else if (pid == 0) {
-        /* Child process. */
-        printf("Running...\n");
-        execlp(command, command, NULL);
-    } else {
-        /* Parent process. */
-        int result;
-        wait(&result);
-        printf("All done; result = %d\n", result);
-    }
+    	//declared variables/ pointers to be used in this loop
+        const char *delimiter = " ";
+        const char *waitCharacter = "&";
+        int waitCharacterPresent = 0;
+        char *argument;
+        char *context;
+
+        //user's first message from shell
+        printf("Enter the command to run: ");
+        fgets(command, 256, stdin);
+
+        int commandLength = strlen(command);
+
+        //Change newline to terminating 0
+        command[commandLength - 1] = 0;
+
+        //Remove any trailing spaces
+        while(strcmp(&command[commandLength - 2], delimiter) == 0){
+            command[commandLength - 2] = 0;
+            commandLength--;
+        }
+
+        //string comparison whether or not to removed the "&" from the 
+        //command or last argument
+        if(strcmp(&command[commandLength - 2], waitCharacter) == 0){
+            command[commandLength - 2] = 0;
+            waitCharacterPresent = 1;
+        }
+
+        //refered to this for strtok_r: http://linux.die.net/man/3/strtok_r
+        //Performs token parsing on arguments
+        for(argument = strtok_r(command, delimiter, &context), i;argument; argument = strtok_r(NULL, delimiter, &context), i++){
+            args[i] = argument;
+        }
+
+        //checks the arguments for "cd" 
+        if(strcmp(args[0], "cd") == 0) {
+
+            chdir(args[1]);
+
+        } else if(strcmp(args[0], "secret-system-call") == 0) {
+           
+            //check system call from assignment 0311
+            syscall(351);
+
+        } else {
+
+            //similar to the original Bazaar code
+            args[i] = NULL;
+
+            /* Variable that will store the fork result. */
+            pid_t pid;
+
+            /* Perform the actual fork. */
+            pid = fork();
+            if (pid < 0) {
+                /* Error condition. */
+                fprintf(stderr, "Fork failed\n");
+                return -1;
+
+            } else if (pid == 0) {
+                /* Child process. */
+                printf("Running...\n");
+                execvp(args[0], args);
+
+            } else {
+                /* Parent process. */
+                int result;
+                if (!waitCharacterPresent) {
+                    wait(&result);
+                }
+                printf("All done; result = %d\n", result);
+            }
+        }
+    } while (!feof(stdin));
 
     return 0;
 }
---
+++ homework/myshell/points-to-ponder.md
@@ -0,0 +1,17 @@
+Points To Ponder:
+=================
+
+1. Why did the cd command get special mention in 
+this assignment? 
+
+    The `cd` command got special mention because `cd` is not an executable command.  
+
+2. Can your shell run scripts? Why or why not? 
+
+    My shell does not run scripts because it only knows how to handle certain executable 
+    commands (e.g. `ls`, `ping`).
+
+3. Does Ctrl-D exit your shell? Why or why not? 
+    
+    Ctrl-D` exits my shell because the while loop keeps going until an `EOF`
+	character is found.
