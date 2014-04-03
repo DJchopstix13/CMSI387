5795b235377aa841eeee5a1cec15d4f5d0290699
Jonathan Piatos <jpiatos13@gmail.com>
2014-04-03 14:14:56 -0700

    got cd to work

--- homework/myshell/fork-exec.c
+++ homework/myshell/fork-exec.c
@@ -7,96 +7,109 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <string.h>
+#include <stdlib.h>
 
-/**
- * This program demonstrates the use of the fork() and exec()
+#define MAX_TKN 10
+#define CMD_LENGTH 256
+#define DIR_SIZE 1024
+
+/*
+ * Tokenizes user input.  This method will 
+ * store string into an array
+ *
+ */
+int tokening_input (char *command, char *arg[]) {
+    
+    //initialize index and delimeter
+    int i = 0;
+    char *delimeter = " \n";
+
+    //handles strtok parsing
+    char *arguments = strtok(command, delimeter);
+
+    while (arguments != NULL) {
+        arg[i] = arguments;
+        arguments = strtok(NULL, delimeter);
+        i++;
+    }
+
+    arg[i] = NULL;
+    return 0;
+}
+
+/*
+ * Demonstrates the use of the fork() and exec()
  * functions.
+ *
  */
 int main() {
 
-    //String to hold the command to run.
-    char command[256];
-    char *args[256];
-    int i;
-
-    //continuous do-while loop
+    //strings to hold commands
+    char command[CMD_LENGTH];
+    char *arg[MAX_TKN];
+    
     do {
+        
+        //Displays current directory
+        char directory[DIR_SIZE];
+        getcwd(directory, sizeof(directory));
+        printf("Enter your command: ", directory);        
 
-    	//declared variables/ pointers to be used in this loop
-        const char *delimiter = " ";
-        const char *waitCharacter = "&";
-        int waitCharacterPresent = 0;
-        char *argument;
-        char *context;
+        fgets(command, sizeof(command), stdin);
 
-        //user's first message from shell
-        printf("Enter the command to run: ");
-        fgets(command, 256, stdin);
+         // Stops everything if the only character is a newline 
+         if (strlen(command) > 1) {
+            
+            //check "&" command
+            int willWait = 1;
+            if (command[strlen(command) - 2] == '&') {
+                command[strlen(command) - 2] = '\0';
+                willWait = 0;
+            }
+        
+            //Parses input
+            tokening_input(command, arg);
+        
+            //Checks "cd" command
+            if (strcmp("cd", arg[0]) == 0) {
+                
+                chdir(arg[1]);
 
-        int commandLength = strlen(command);
+            } 
 
-        //Change newline to terminating 0
-        command[commandLength - 1] = 0;
-
-        //Remove any trailing spaces
-        while(strcmp(&command[commandLength - 2], delimiter) == 0){
-            command[commandLength - 2] = 0;
-            commandLength--;
-        }
-
-        //string comparison whether or not to removed the "&" from the 
-        //command or last argument
-        if(strcmp(&command[commandLength - 2], waitCharacter) == 0){
-            command[commandLength - 2] = 0;
-            waitCharacterPresent = 1;
-        }
-
-        //refered to this for strtok_r: http://linux.die.net/man/3/strtok_r
-        //Performs token parsing on arguments
-        for(argument = strtok_r(command, delimiter, &context), i;argument; argument = strtok_r(NULL, delimiter, &context), i++){
-            args[i] = argument;
-        }
-
-        //checks the arguments for "cd" 
-        if(strcmp(args[0], "cd") == 0) {
-
-            chdir(args[1]);
-
-        } else if(strcmp(args[0], "secret-system-call") == 0) {
-           
-            //check system call from assignment 0311
-            syscall(351);
-
-        } else {
-
-            //similar to the original Bazaar code
-            args[i] = NULL;
-
-            /* Variable that will store the fork result. */
-            pid_t pid;
-
-            /* Perform the actual fork. */
-            pid = fork();
-            if (pid < 0) {
-                /* Error condition. */
-                fprintf(stderr, "Fork failed\n");
-                return -1;
-
-            } else if (pid == 0) {
-                /* Child process. */
-                printf("Running...\n");
-                execvp(args[0], args);
-
+            //Checks for secret-system-call
+            else if (strcmp("secret-system-call", arg[0]) == 0) { 
+                
+                long int call = syscall(351);
+                printf("Syscall 351 invoked.\n\n");
+            
             } else {
-                /* Parent process. */
-                int result;
-                if (!waitCharacterPresent) {
-                    wait(&result);
+                //based off of Bazaar's original fork-exec.c file
+                
+                /* Variable that will store the fork result */
+                pid_t pid;
+
+                /* Perform the actual fork */
+                pid = fork();
+                if (pid  < 0) {
+                    /* Error condition */
+                    fprintf(stderr, "Fork failed\n");
+                    return -1;
+                } else if (pid == 0) {
+                    /* Child process */
+                    printf("\n");
+                    execvp(arg[0], arg);
+                } else {
+                    /* Parent Process */
+                    int result;
+                    if (willWait == 1) {
+                        wait(&result);
+                    }
+                    printf("\nAll done; result = %d\n\n", result);
                 }
-                printf("All done; result = %d\n", result);
             }
         }
     } while (!feof(stdin));
 
     return 0;
-}
+} 
